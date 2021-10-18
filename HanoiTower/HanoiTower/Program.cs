68b7a882c4eace971504738a6d5a8d07745bec3b
Omaenoshi <rassvetalov.maksim@mail.ru>
2021-10-19 03:16:27 +0500

    add code

---
+++ HanoiTower/HanoiTower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HanoiTower
+{
+    class Program
+    {
+        static void Move(int start, int destination, int buffer, int count)
+        {
+            if (count != 0)
+            {
+                Move(start, buffer, destination, count - 1);
+                Console.WriteLine($"| from {start} to {destination}");
+                Move(buffer, destination, start, count - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Move(1, 3, 2, 4);
+        }
+    }
+}
